8e455db72ff70d7be544ac939b6e043386bf4ba7
Timothy Tan <timtan93@gmail.com>
2018-11-28 14:41:40 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  my_collect_2 = []
+  i = 0
+    while i < collection.length
+      my_collect_two << yield(collection[i])
+      i = i + 1
+    end
+  my_collect_2
+end
